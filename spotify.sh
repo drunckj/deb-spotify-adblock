f938ef0d39e0cd564f85b50f534d72821c8e1d20
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-10-06 20:58:32 +0530

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -4,9 +4,9 @@ sudo apt-get update && sudo apt-get install spotify-client
 git clone https://github.com/abba23/spotify-adblock.git
 cd spotify-adblock/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source .bashrc
+source $HOME/.cargo/env
 make
 sudo make install
-sudo cp spotify-adblock.desktop ~/.local/share/applications/
+sudo cp spotify-adblock.desktop /usr/share/applications/
 
 
