975e83830b8fd3d1f02c7ce4ecc86d9788492f31
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2021-10-06 20:50:16 +0530

    Add files via upload

---
+++ spotify.sh
@@ -0,0 +1,12 @@
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+git clone https://github.com/abba23/spotify-adblock.git
+cd spotify-adblock/
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source .bashrc
+make
+sudo make install
+sudo cp spotify-adblock.desktop ~/.local/share/applications/
+
+
