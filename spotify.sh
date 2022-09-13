06fc4dae5a6fe09014b2004f0fbd27f98594ef7f
Swastik Dwivedi <50874358+drunckj@users.noreply.github.com>
2022-09-13 18:06:25 +0530

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -3,7 +3,7 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg -
 echo "deb [signed-by=/usr/share/keyrings/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client
 git clone https://github.com/abba23/spotify-adblock.git
-cd spotify-adblock/spotify-client
+cd spotify-adblock
 if [ ! -f "$HOME/.cargo/bin/rustup" ]
 then
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
